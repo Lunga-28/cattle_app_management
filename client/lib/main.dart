895e2295ce723fe9df57032c57d9eee1dd9548ce
Lunga28 <nokulungamkize28@gmail.com>
2024-12-19 12:34:50 +0200

    redefine the UI

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,17 +1,45 @@
-import 'package:cattle_management_app/home.dart';
 import 'package:flutter/material.dart';
+import 'package:poultry_farm_app/staff/staff_page.dart';
+import 'welcome_screen.dart';
+import 'utils/color_utils.dart';
+import 'screens/dashboard_screen.dart';
+import 'login_screen.dart';
 
 void main() {
-  runApp( MyApp());
+  runApp(CattleFarmApp());
 }
 
-class MyApp extends StatelessWidget {
+class CattleFarmApp extends StatelessWidget {
+  const CattleFarmApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:  Home(),
-// Set the theme to light mode
+      initialRoute: '/',
+      onGenerateRoute: (settings) {
+        if (settings.name == '/dashboard') {
+          final String farmId = settings.arguments as String;
+          return MaterialPageRoute(
+            builder: (context) => DashboardScreen(farmId: farmId),
+          );
+        } else if (settings.name == '/finances') {
+          final String farmId = settings.arguments as String;
+          return MaterialPageRoute(
+            builder: (context) => FinancePage(farmId: farmId),
+          );
+        }
+        return null;
+      },
+      routes: {
+        '/': (context) => const WelcomeScreen(),
+        '/login': (context) => const loginScreen(),
+      },
+      theme: ThemeData(
+        primarySwatch: createMaterialColor(const Color.fromARGB(255, 145, 0, 150)),
+        fontFamily: 'Roboto',
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
     );
   }
 }
